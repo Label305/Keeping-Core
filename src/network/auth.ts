ef792372b55f92f41314d34f94d8f236beab5287
Dirk Jan Wassink <dirk.jan.wassink@gmail.com>
2022-02-22 14:55:47 +0100

    Added authentication by username & password call

--- src/network/auth.ts
+++ src/network/auth.ts
@@ -3,25 +3,37 @@ import Axios from 'axios';
 import {Credentials} from '../models/credentials';
 
 export class AuthApi {
-    constructor(
-        private keepingApiUrl: string,
-        private keepingClientId: string,
-        private keepingClientSecret: string,
-        private redirectUrl: string,
-    ) {}
+    constructor(private keepingApiUrl: string, private keepingClientId: string, private keepingClientSecret: string) {}
 
     /**
      * Fetch credentials from authorization code
      * @param code
      * @returns Promise<Credentials>
      */
-    public async byCode(code: string): Promise<Credentials> {
+    public async byCode(code: string, redirectUri: string): Promise<Credentials> {
         const response = await Axios.create().post(`${this.keepingApiUrl}/oauth/token`, {
             grant_type: 'authorization_code',
             client_id: this.keepingClientId,
             client_secret: this.keepingClientSecret,
-            redirect_uri: this.redirectUrl,
+            redirect_uri: redirectUri,
             code: code,
+        });
+        return response.data as Credentials;
+    }
+
+    /**
+     * Fetch credentials with username & password
+     * @param username
+     * @param password
+     * @returns Promise<Credentials>
+     */
+    public async byUsernameAndPassword(username: string, password: string): Promise<Credentials> {
+        const response = await Axios.create().post(`${this.keepingApiUrl}/oauth/token`, {
+            grant_type: 'password',
+            client_id: this.keepingClientId,
+            client_secret: this.keepingClientSecret,
+            username: username,
+            password: password,
         });
         return response.data as Credentials;
     }
@@ -32,7 +44,7 @@ export class AuthApi {
      * @returns Promise<Credentials>
      */
     public async byRefreshToken(refreshToken: string): Promise<Credentials> {
-        const response = await Axios.create().post(this.keepingApiUrl + '/oauth/token', {
+        const response = await Axios.create().post(`${this.keepingApiUrl}/oauth/token`, {
             grant_type: 'refresh_token',
             client_id: this.keepingClientId,
             client_secret: this.keepingClientSecret,
